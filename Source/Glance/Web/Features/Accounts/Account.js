a6ecc129b76eab52bf44041549d7059f9f10c748
Einar Ingebrigtsen <einar@dolittle.com>
2020-01-18 09:43:53 +0100

    New proxy

--- Source/Glance/Web/Features/Accounts/Account.js
+++ Source/Glance/Web/Features/Accounts/Account.js
@@ -9,12 +9,12 @@ export class Account extends ReadModel
     constructor() {
         super();
         this.artifact = {
-           id: '87344b03-93a2-4ea8-b2bf-deb2250b3f9b',
+           id: '3b6d847a-45c7-4381-a53a-df6b29be1556',
            generation: '1'
         };
         this.id = '00000000-0000-0000-0000-000000000000';
         this.customerId = '00000000-0000-0000-0000-000000000000';
-        this.type = 0;
+        this.name = '';
         this.balance = 0;
     }
 }
